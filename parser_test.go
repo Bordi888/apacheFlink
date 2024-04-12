34ce17b2c363fa72e023896c3619e59526c2c3db
Bordi888 <saidbordi@aol.com>
2024-04-12 17:25:42 +0300

    Update dependencies

---
+++ parser_test.go
@@ -0,0 +1,104 @@
+package seq
+
+import "testing"
+import "strings"
+import "bufio"
+
+func TestForwardArrow(t *testing.T) {
+
+	s := bufio.NewReader(strings.NewReader("A -> B: Test\nC->D:More"))
+	c := make(StatementFeed)
+
+	p := NewParser(c)
+	p.Parse(s)
+
+	var x *Statement
+
+	x = <-c
+	if x.from != "A" {
+		t.Error("From property incorrect")
+	}
+	if x.to != "B" {
+		t.Error("To property incorrect")
+	}
+	if x.label != "Test" {
+		t.Error("Label incorrect")
+	}
+
+	x = <-c
+	if x.from != "C" {
+		t.Error("From property incorrect")
+	}
+	if x.to != "D" {
+		t.Error("To property incorrect")
+	}
+	if x.label != "More" {
+		t.Error("Label incorrect")
+	}
+}
+
+func TestBackArrow(t *testing.T) {
+
+	s := bufio.NewReader(strings.NewReader("A <- B: Test\nC<-D:More"))
+	c := make(StatementFeed)
+
+	p := NewParser(c)
+	p.Parse(s)
+
+	var x *Statement
+
+	x = <-c
+	if x.from != "B" {
+		t.Error("From property incorrect")
+	}
+	if x.to != "A" {
+		t.Error("To property incorrect")
+	}
+	if x.label != "Test" {
+		t.Error("Label incorrect")
+	}
+
+	x = <-c
+	if x.from != "D" {
+		t.Error("From property incorrect")
+	}
+	if x.to != "C" {
+		t.Error("To property incorrect")
+	}
+	if x.label != "More" {
+		t.Error("Label incorrect")
+	}
+}
+
+func TestLoopbackArrow(t *testing.T) {
+
+	s := bufio.NewReader(strings.NewReader("A->A: Test\nB->B:More"))
+	c := make(StatementFeed)
+
+	p := NewParser(c)
+	p.Parse(s)
+
+	var x *Statement
+
+	x = <-c
+	if x.from != "A" {
+		t.Error("From property incorrect")
+	}
+	if x.to != "A" {
+		t.Error("To property incorrect")
+	}
+	if x.label != "Test" {
+		t.Error("Label incorrect")
+	}
+
+	x = <-c
+	if x.from != "B" {
+		t.Error("From property incorrect")
+	}
+	if x.to != "B" {
+		t.Error("To property incorrect")
+	}
+	if x.label != "More" {
+		t.Error("Label incorrect")
+	}
+}
