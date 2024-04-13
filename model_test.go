e5f1dc2354ed3995fae5c0236b156fb38a283686
Bordi888 <saidbordi@aol.com>
2024-04-13 17:25:42 +0300

    Add new feature for user avatars

---
+++ model_test.go
@@ -0,0 +1,43 @@
+package seq
+
+import "testing"
+import "fmt"
+
+var _ = fmt.Printf
+
+func TestStatementGetParticipants(t *testing.T) {
+	s := &Statement{from: participant("A"), to: participant("B"), label: "Label"}
+
+	participants := s.getParticipants()
+
+	if participants[0] != "A" {
+		t.Error("First participant of the statement was wrong.")
+	}
+	if participants[1] != "B" {
+		t.Error("Second participant of the statement was wrong.")
+	}
+}
+
+func TestExtractParticipants(t *testing.T) {
+
+	s1 := &Statement{from: "A", to: "B", label: "Label"}
+	s2 := &Statement{from: "C", to: "D", label: "Other"}
+
+	d := &diagram{}
+
+	d.addStatement(s1)
+	d.addStatement(s2)
+
+	if d.Participants[0] != "A" {
+		t.Error("First partipant is wrong.")
+	}
+	if d.Participants[1] != "B" {
+		t.Error("Second partipant is wrong.")
+	}
+	if d.Participants[2] != "C" {
+		t.Error("Third partipant is wrong.")
+	}
+	if d.Participants[3] != "D" {
+		t.Error("Fourth partipant is wrong.")
+	}
+}
